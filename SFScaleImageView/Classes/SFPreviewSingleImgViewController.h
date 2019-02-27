7c1007a26859704341ed9ea78a38d05653340097
liuxuleidota <liu_xu_lei@163.com>
2019-02-27 15:44:34 +0800

    代码提交

--- SFScaleImageView/Classes/SFPreviewSingleImgViewController.h
+++ SFScaleImageView/Classes/SFPreviewSingleImgViewController.h
@@ -16,4 +16,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+@interface UIView (Helper)
+
+@property (nonatomic) CGFloat left;        ///< Shortcut for frame.origin.x.
+@property (nonatomic) CGFloat top;         ///< Shortcut for frame.origin.y
+@property (nonatomic) CGFloat right;       ///< Shortcut for frame.origin.x + frame.size.width
+@property (nonatomic) CGFloat bottom;      ///< Shortcut for frame.origin.y + frame.size.height
+@property (nonatomic) CGFloat width;       ///< Shortcut for frame.size.width.
+@property (nonatomic) CGFloat height;      ///< Shortcut for frame.size.height.
+@property (nonatomic) CGFloat centerX;     ///< Shortcut for center.x
+@property (nonatomic) CGFloat centerY;     ///< Shortcut for center.y
+
+@end
+
 NS_ASSUME_NONNULL_END
